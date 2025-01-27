c3472a19d6bf450e129be6b549841fa6463ca9e0
Andrey22 <andreymail22112006@gmail.com>
2025-01-27 13:20:04 +0300

    Fix CalendarQuickstart

--- src/main/java/dev/madela/hr_bot/CalendarQuickstart.java
+++ src/main/java/dev/madela/hr_bot/CalendarQuickstart.java
@@ -8,7 +8,6 @@ import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
-import com.google.api.services.calendar.model.Events;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 
@@ -19,13 +18,13 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.Collections;
-import java.util.List;
 
 public class CalendarQuickstart {
     private static final String APPLICATION_NAME = "Google Calendar API HRBot";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static Calendar service;
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
+
+    public static void createEvent(String title, String start, String end) throws IOException, GeneralSecurityException {
         FileInputStream credentialsStream = new FileInputStream("credentials.json");
         GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream)
                 .createScoped(Collections.singletonList(CalendarScopes.CALENDAR));
@@ -34,34 +33,7 @@ public class CalendarQuickstart {
         service = new Calendar.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, new HttpCredentialsAdapter(credentials))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
-        listUpcomingEvents();
-        createEvent("Тест", "2025-01-26T19:00:00", "2025-01-27T22:00:00");
-    }
 
-    private static void listUpcomingEvents() throws IOException {
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("7b40dfb7d6086a335ee17ff269dae830e74542ef7d6b3e62d6441c55b26603b8@group.calendar.google.com")
-                .setMaxResults(10)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .setTimeMin(now)
-                .execute();
-        List<Event> items = events.getItems();
-        if (items.isEmpty()) {
-            System.out.println("Предстоящих событий не найдено");
-        } else {
-            System.out.println("Предстоящие события:");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                if (start == null) {
-                    start = event.getStart().getDate();
-                }
-                System.out.printf("%s (%s)\n", event.getSummary(), start);
-            }
-        }
-    }
-
-    public static void createEvent(String title, String start, String end) throws IOException {
         ZoneId zoneId = ZoneId.of(ZoneId.systemDefault().toString());
         ZoneOffset offset = zoneId.getRules().getOffset(Instant.now());
         String offsetString = offset.getId();
