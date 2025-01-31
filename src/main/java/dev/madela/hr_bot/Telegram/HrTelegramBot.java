ebdd5893de954579a441a1711b87737b289a3575
mariapigina6162@gmail.com <mariapigina6162@gmail.com>
2025-01-31 15:03:03 +0300

    импорт удален

--- src/main/java/dev/madela/hr_bot/Telegram/HrTelegramBot.java
+++ src/main/java/dev/madela/hr_bot/Telegram/HrTelegramBot.java
@@ -1,6 +1,5 @@
 package dev.madela.hr_bot.Telegram;
 
-import dev.madela.hr_bot.CreateGoogleCalendarEventService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
