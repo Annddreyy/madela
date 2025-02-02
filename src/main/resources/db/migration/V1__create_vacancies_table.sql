d5dc6bb669b3cbeb060bcc5ae2de3c4cbfe28870
venom_play <idaniil535@gmail.com>
2025-02-02 19:35:49 +0300

    added migration

--- src/main/resources/db/migration/V1__create_vacancies_table.sql
+++ src/main/resources/db/migration/V1__create_vacancies_table.sql
@@ -1,7 +1,22 @@
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(20) NOT NULL
+    );
+
     CREATE TABLE vacancies (
         id SERIAL PRIMARY KEY,
         title VARCHAR(100) NOT NULL,
         description TEXT NOT NULL,
-        salary NUMERIC NOT NULL
+        salary NUMERIC NOT NULL,
+        posted_by INTEGER REFERENCES users(id)
+    );
+
+    CREATE TABLE faqs (
+        id SERIAL PRIMARY KEY,
+        question TEXT NOT NULL,
+        answer TEXT NOT NULL
     );
 
